723999356dd0343be8f09676b067510caf7f22a6
Sara Szwejkowska <saraszw@op.pl>
2022-11-27 17:52:42 +0100

    created functions to buttons& activated the buttons

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const firstBox = document.querySelector(".firstBox")
-const secondBox = document.querySelector(".secondBox")
-const submitButton = document.querySelector(".submit")
-const ratings = document.getElementById("ratings")
-const rates = document.querySelectorAll(".btn")
+const firstBox = document.querySelector('.firstBox');
+const secondBox = document.querySelector('.secondBox');
+const submitButton = document.querySelector('.submit');
+const ratings = document.getElementById('ratings');
+const rates = document.querySelectorAll('.btn');
 
-
-
-
-submitButton.addEventListener("click", () => {
-    secondBox.classList.remove("hidden")
-    firstBox.style.display = "none"
-})
+submitButton.addEventListener('click', () => {
+	if (ratings.innerHTML === '') {
+		alert('bug');
+	} else {
+		secondBox.classList.remove('hidden');
+		firstBox.style.display = 'none';
+	}
+});
 
 rates.forEach((rate) => {
-rate.addEventListener("click", ()=> {
-ratings.innerHTML=rate.innerHTML
-})
-}
-)
+	rate.addEventListener('click', () => {
+		rates.forEach((el) => {
+			if (el.classList.contains('active')) {
+				el.classList.remove('active');
+			}
+		});
+		ratings.innerHTML = rate.innerHTML;
+		rate.classList.add('active');
+	});
+});
